c869dc072ea51b84ca00fb0a15b48b9621058ee1
MelDv <maaret.dufva@gmail.com>
2016-12-16 22:29:52 +0200

    test

---
+++ src/test/java/wad/TypetutorTest.java
@@ -0,0 +1,14 @@
+package wad;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TypetutorTest {
+
+    @Test
+    public void testStuff() {
+        // note; this is an assignment with no tests -- make sure
+        // that your application works as intended before submitting it
+        assertTrue(1 + 1 == 2);
+    }
+}
