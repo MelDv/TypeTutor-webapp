17324a9a6349db01b9f7b377f5e964ef16ed2cf5
MelDv <maaret.dufva@gmail.com>
2016-12-17 20:13:28 +0200

    fixes

--- src/main/java/wad/ProductionProfile.java
+++ src/main/java/wad/ProductionProfile.java
@@ -16,14 +16,12 @@ public class ProductionProfile {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
         String username = dbUri.getUserInfo().split(":")[0];
-        String email = dbUri.getUserInfo().split(":")[1];
-        String password = dbUri.getUserInfo().split(":")[2];
+        String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
         basicDataSource.setUsername(username);
-        basicDataSource.setPassword(email);
         basicDataSource.setPassword(password);
 
         return basicDataSource;
