f2b1223a4bc128826c3b5e889d9ef7e2c0cd4eca
MelDv <maaret.dufva@gmail.com>
2016-12-17 20:59:50 +0200

    fixed deleting users

--- src/main/java/wad/repository/AccountRepository.java
+++ src/main/java/wad/repository/AccountRepository.java
@@ -5,8 +5,8 @@ import wad.domain.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-//    @Override
-//    public void delete(Long id);
+    @Override
+    public void delete(Long id);
 
     Account findByUsername(String username);
 }
